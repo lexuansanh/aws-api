1e3edd61880d768f7386648b222f9a7e99dd6a9d
SanhLx <xuansanh96@gmail.com>
2022-05-25 23:55:35 +0700

    add my files

---
+++ todos/create.py
@@ -0,0 +1,36 @@
+import json
+import logging
+import os
+import time
+import uuid
+
+import boto3
+dynamodb = boto3.resource('dynamodb')
+
+
+def create(event, context):
+    data = json.loads(event['body'])
+    if 'name' not in data:
+        logging.error("Validation Failed")
+        raise Exception("Couldn't create the todo item.")
+    
+    timestamp = str(time.time())
+
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    item = {
+        'id': str(uuid.uuid1()),
+        'name': data['name'],
+        'location': data['location']
+    }
+
+    # write the todo to the database
+    table.put_item(Item=item)
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(item)
+    }
+
+    return response
